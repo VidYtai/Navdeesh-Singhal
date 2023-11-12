00b7bf3ed9858ecc8275da7da31cf86bac025129
Navdeesh <136051807+VidYtai@users.noreply.github.com>
2023-11-12 12:05:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: rgb(22, 162, 203);">
   <a href="/" target="_blank" rel="noopener noreferrer" class="navbar-brand">Ds. Navdeesh</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
