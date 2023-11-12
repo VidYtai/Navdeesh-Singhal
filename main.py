8161002cdda01be8f673c5b4e1d3ae3cb075b64d
Navdeesh <136051807+VidYtai@users.noreply.github.com>
2023-11-12 12:09:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ st.markdown("""
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home</a>
+        <a class="nav-link" href="#navdeesh-data-scientist">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
