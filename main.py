63d1e13445a72bbab061479f73c78a3b71eb0e55
Navdeesh <136051807+VidYtai@users.noreply.github.com>
2023-11-25 12:19:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ st.set_page_config(
 )
 
 st.markdown(
-    '''<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">''',
+    '''<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3150291707916239"
+     crossorigin="anonymous"></script>''',
     unsafe_allow_html=True)
 
 st.markdown("""
